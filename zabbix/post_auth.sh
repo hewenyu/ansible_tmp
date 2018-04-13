c9c64ccb99abe61525f8bb76bd4c27802bcd1bef
yueban <410496058@qq.com>
2018-04-13 14:16:32 +0800

    获取token

--- zabbix/post_auth.sh
+++ zabbix/post_auth.sh
@@ -4,4 +4,4 @@ api_url="http://127.0.0.1/zabbix/api_jsonrpc.php"
 # 编辑json
 post_data=`cat login.json`
 get_date=`curl -s -H "Content-Type: application/json" -X POST  --data "${post_data}" "${api_url}"`
-echo $get_date
+echo $get_date |awk -F '"' '{print $(8)}' > token.json
