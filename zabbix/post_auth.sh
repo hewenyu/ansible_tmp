6fa593ec15b39ad96d382583444e88a92021e1dd
yueban <410496058@qq.com>
2018-04-13 14:00:31 +0800

    脚本修改

--- zabbix/post_auth.sh
+++ zabbix/post_auth.sh
@@ -3,4 +3,5 @@
 api_url="http://127.0.0.1/zabbix/api_jsonrpc.php"
 # 编辑json
 post_data=`cat login.json`
-curl -i -H "Content-Type: application/json" -X POST  --data "${post_data}" "${api_url}"
+get_date=`curl -s -H "Content-Type: application/json" -X POST  --data "${post_data}" "${api_url}"`
+echo $get_date
